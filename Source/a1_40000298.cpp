194265fec89f35ab302b0ae72dbf5ca90aac642c
LHuss <loic.huss@mail.concordia.ca>
2019-10-04 22:21:30 -0400

    initial commit(a1): Stripped lab 3 to its barebones

---
+++ Source/a1_40000298.cpp
@@ -0,0 +1,368 @@
+//
+// COMP 371 Labs Framework
+//
+// Created by Nicolas Bergeron on 20/06/2019.
+// Used as baseline for Assignment 1 by Loïc Huss on 02/10/2019
+//
+
+#include <iostream>
+#include <list>
+#include <algorithm>
+
+#define GLEW_STATIC 1   // This allows linking with Static Library on Windows, without DLL
+#include <GL/glew.h>    // Include GLEW - OpenGL Extension Wrangler
+
+#include <GLFW/glfw3.h> // cross-platform interface for creating a graphical context,
+                        // initializing OpenGL and binding inputs
+
+#include <glm/glm.hpp>  // GLM is an optimized math library with syntax to similar to OpenGL Shading Language
+#include <glm/gtc/matrix_transform.hpp> // include this to create transformation matrices
+#include <glm/common.hpp>
+
+
+using namespace glm;
+using namespace std;
+
+const char* getVertexShaderSource()
+{
+    // For now, you use a string for your shader code, in the assignment, shaders will be stored in .glsl files
+    return
+        "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;"
+        "layout (location = 1) in vec3 aColor;"
+        ""
+        "uniform mat4 worldMatrix;"
+        "uniform mat4 viewMatrix = mat4(1.0);"  // default value for view matrix (identity)
+        "uniform mat4 projectionMatrix = mat4(1.0);"
+        ""
+        "out vec3 vertexColor;"
+        "void main()"
+        "{"
+        "   vertexColor = aColor;"
+        "   mat4 modelViewProjection = projectionMatrix * viewMatrix * worldMatrix;"
+        "   gl_Position = modelViewProjection * vec4(aPos.x, aPos.y, aPos.z, 1.0);"
+        "}";
+}
+
+const char* getFragmentShaderSource()
+{
+    return
+        "#version 330 core\n"
+        "in vec3 vertexColor;"
+        "out vec4 FragColor;"
+        "void main()"
+        "{"
+        "   FragColor = vec4(vertexColor.r, vertexColor.g, vertexColor.b, 1.0f);"
+        "}";
+}
+
+int compileAndLinkShaders()
+{
+    // compile and link shader program
+    // return shader program id
+    // ------------------------------------
+
+    // vertex shader
+    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    const char* vertexShaderSource = getVertexShaderSource();
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    
+    // check for shader compile errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cerr << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    
+    // fragment shader
+    int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    const char* fragmentShaderSource = getFragmentShaderSource();
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    
+    // check for shader compile errors
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cerr << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    
+    // link shaders
+    int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    
+    // check for linking errors
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cerr << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+    
+    return shaderProgram;
+}
+
+int createVertexBufferObject()
+{
+	    
+    // Create a vertex array
+    GLuint vertexArrayObject;
+    glGenVertexArrays(1, &vertexArrayObject);
+    glBindVertexArray(vertexArrayObject);
+    
+    
+    // Upload Vertex Buffer to the GPU, keep a reference to it (vertexBufferObject)
+    GLuint vertexBufferObject;
+
+    
+    return vertexBufferObject;
+}
+
+
+int main(int argc, char*argv[])
+{
+    // Initialize GLFW and OpenGL version
+    glfwInit();
+    
+#if defined(PLATFORM_OSX)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#else
+    // On windows, we set OpenGL version to 2.1, to support more hardware
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+#endif
+
+    // Create Window and rendering context using GLFW, resolution is 800x600
+    GLFWwindow* window = glfwCreateWindow(1024, 768, "Comp371 - Lab 03", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Disable Mouse Cursor
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    
+    // Initialize GLEW
+    glewExperimental = true; // Needed for core profile
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "Failed to create GLEW" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // Black background
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    
+    // Compile and link shaders here ...
+    int shaderProgram = compileAndLinkShaders();
+    
+    // We can set the shader once, since we have only one
+    glUseProgram(shaderProgram);
+
+    // Camera parameters for view transform
+    vec3 cameraPosition(10.0f,10.0f,10.0f);
+    vec3 cameraLookAt(0.0f, 0.0f, 0.0f);
+    vec3 cameraUp(0.0f, 1.0f, 0.0f);
+    
+    // Other camera parameters
+    float cameraSpeed = 1.0f;
+    float cameraFastSpeed = 20 * cameraSpeed; // 20 shift speed cuz gotta go fast boiiii
+    float cameraHorizontalAngle = 90.0f;
+    float cameraVerticalAngle = 0.0f;
+    bool cameraFirstPerson = true; // press 1 or 2 to toggle this variable, or f5 to toggle
+	double lastCameraToggle = -300;
+	float minWaitToggle = 0.250f;
+	    
+    // Set projection matrix for shader
+    mat4 projectionMatrix = glm::perspective(90.0f,            // field of view in degrees (90 to feel fast af)
+                                             1024.0f / 768.0f,  // aspect ratio
+                                             0.01f, 100.0f);   // near and far (near > 0)
+    
+    GLuint projectionMatrixLocation = glGetUniformLocation(shaderProgram, "projectionMatrix");
+    glUniformMatrix4fv(projectionMatrixLocation, 1, GL_FALSE, &projectionMatrix[0][0]);
+
+    // Set initial view matrix
+    mat4 viewMatrix = lookAt(cameraPosition,				 // eye
+                             cameraPosition + cameraLookAt,  // center
+                             cameraUp );					 // up
+    
+    GLuint viewMatrixLocation = glGetUniformLocation(shaderProgram, "viewMatrix");
+    glUniformMatrix4fv(viewMatrixLocation, 1, GL_FALSE, &viewMatrix[0][0]);
+
+    
+    
+    // Define and upload geometry to the GPU here ...
+    int vbo = createVertexBufferObject();
+    
+    // For frame time
+    float lastFrameTime = glfwGetTime();
+    int lastMouseLeftState = GLFW_RELEASE;
+    double lastMousePosX, lastMousePosY;
+    glfwGetCursorPos(window, &lastMousePosX, &lastMousePosY);
+    
+    // Enable hidden surface removal
+    glEnable(GL_CULL_FACE);
+
+    // Entering Main Loop
+    while(!glfwWindowShouldClose(window))
+    {
+        // Frame time calculation
+        float dt = glfwGetTime() - lastFrameTime;
+        lastFrameTime += dt;
+
+		glEnable(GL_DEPTH_TEST); 
+		// Add the GL_DEPTH_BUFFER_BIT to glClear - Basically just draw close objects on top
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Draw geometry
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+        // Draw grid
+        
+
+        // Draw in view space for first person camera
+		// TODO
+		// ADD SPINNING CAR IN VIEW FOR SHITS N GIGGLES
+        if (cameraFirstPerson){
+		}
+		else {
+		}
+
+        
+        
+        // End Frame
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        
+        // Handle inputs
+        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, true);
+        
+		// this does strictly nothing as of yet, since we have no way to differentiate first/third person for now
+        if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS) // change to first person
+        {
+            cameraFirstPerson = true;
+        }
+
+        if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS) // change to third person
+        {
+            cameraFirstPerson = false;
+        }
+
+		if (glfwGetKey(window, GLFW_KEY_F5) == GLFW_PRESS) // toggle camera state
+		{
+			double curTime = glfwGetTime();
+			if ((curTime - minWaitToggle) > lastCameraToggle) {
+				cameraFirstPerson = !cameraFirstPerson;
+				lastCameraToggle = curTime;
+			}
+		}
+
+        // We'll change this to be a first or third person camera
+        bool fastCam = glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_RIGHT_SHIFT) == GLFW_PRESS;
+        float currentCameraSpeed = (fastCam) ? cameraFastSpeed : cameraSpeed;
+        
+        
+        // Calculate mouse motion dx and dy
+        // Update camera horizontal and vertical angle
+        double mousePosX, mousePosY;
+        glfwGetCursorPos(window, &mousePosX, &mousePosY);
+        
+        double dx = mousePosX - lastMousePosX;
+        double dy = mousePosY - lastMousePosY;
+        
+        lastMousePosX = mousePosX;
+        lastMousePosY = mousePosY;
+        
+        // Convert to spherical coordinates
+        const float cameraAngularSpeed = 60.0f;
+        cameraHorizontalAngle -= dx * cameraAngularSpeed * dt;
+        cameraVerticalAngle   -= dy * cameraAngularSpeed * dt;
+        
+        // Clamp vertical angle to [-85, 85] degrees
+        cameraVerticalAngle = std::max(-85.0f, std::min(85.0f, cameraVerticalAngle));
+        if (cameraHorizontalAngle > 360)
+        {
+            cameraHorizontalAngle -= 360;
+        }
+        else if (cameraHorizontalAngle < -360)
+        {
+            cameraHorizontalAngle += 360;
+        }
+        
+        float theta = radians(cameraHorizontalAngle);
+        float phi = radians(cameraVerticalAngle);
+        
+        cameraLookAt = vec3(cosf(phi)*cosf(theta), sinf(phi), -cosf(phi)*sinf(theta));
+        vec3 cameraSideVector = glm::cross(cameraLookAt, vec3(0.0f, 1.0f, 0.0f));
+        
+        glm::normalize(cameraSideVector);
+         
+        
+        // use camera lookat and side vectors to update positions with ASDW
+		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) // move camera down
+		{
+			cameraPosition += cameraLookAt * currentCameraSpeed * dt;
+		}
+
+		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) // move camera up
+		{
+			cameraPosition -= cameraLookAt * currentCameraSpeed * dt;
+		}
+
+        if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) // move camera to the left
+        {
+			cameraPosition -= cameraSideVector * currentCameraSpeed * dt;
+        }
+        
+        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) // move camera to the right
+        {
+			cameraPosition += cameraSideVector * currentCameraSpeed * dt;
+        }
+
+        // Set the view matrix for first and third person cameras
+        // - In first person, camera lookat is set like below
+        // - In third person, camera position is on a sphere looking towards center
+		mat4 viewMatrix(1.0f);
+
+		if (cameraFirstPerson) {
+			viewMatrix = lookAt(cameraPosition, cameraPosition + cameraLookAt, cameraUp);
+		}
+		else {
+			// Position of the camera is on the sphere looking at the point of interest (cameraPosition)
+			float radius = 5.0f;
+			vec3 position = cameraPosition - vec3(
+					radius * cosf(phi) * cosf(theta),
+					radius * sinf(phi),
+					-radius * cosf(phi) * sinf(theta)
+				);
+			viewMatrix = lookAt(position, cameraPosition, cameraUp);
+		}
+
+		GLuint viewMatrixLocation = glGetUniformLocation(shaderProgram, "viewMatrix");
+		glUniformMatrix4fv(viewMatrixLocation, 1, GL_FALSE, &viewMatrix[0][0]);
+        
+		// TODO - ADD PROJECTILE CARS
+    }
+
+    
+    // Shutdown GLFW
+    glfwTerminate();
+    
+	return 0;
+}
